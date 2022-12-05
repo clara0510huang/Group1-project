83e0396434b7bbd515c282b4296454c8d0b94c4d
tsa1chen <116290169+tsa1chen@users.noreply.github.com>
2022-12-04 19:05:46 -0500

    Update CounterClockwise.java

--- Assignment2/model/CounterClockwise.java
+++ Assignment2/model/CounterClockwise.java
@@ -2,6 +2,11 @@ package model;
 
 import java.util.ArrayList;
 
+/**
+ * Counterclockwise
+ *
+ * A class to make a Tetris Piece rotate 90 in counterclockwise.
+ */
 public class CounterClockwise implements IRotation{
     @Override
     public TetrisPiece computeNextRotation(TetrisPiece piece) {
