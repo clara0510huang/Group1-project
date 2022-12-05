38d0b66db1a7f22809167f0e4fb38c0e43d8b157
tsa1chen <116290169+tsa1chen@users.noreply.github.com>
2022-12-04 19:20:33 -0500

    Update IMode.java

--- views/IMode.java
+++ views/IMode.java
@@ -5,6 +5,12 @@ import javafx.scene.layout.BorderPane;
 
 import java.awt.*;
 
+/**
+* IMode strategy interface
+*/
 public interface IMode {
+    /**
+	 * @param view the Tetris view
+	 */
     public void setMode(TetrisView view);
 }
