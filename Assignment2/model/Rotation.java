6d9f95f87f8302f7d6e3b4b236a7fc283ea3d60e
tsa1chen <116290169+tsa1chen@users.noreply.github.com>
2022-12-04 19:14:20 -0500

    Update Rotation.java

--- Assignment2/model/Rotation.java
+++ Assignment2/model/Rotation.java
@@ -1,13 +1,22 @@
 package model;
 
+/**
+* The Rotation defines the interface of interest to clients.
+*/
 public class Rotation {
 
     private IRotation rotationStrategy;
-
+    
+    /**
+    * The Rotation accepts a strategy through the constructor, and also provides a setter so that the strategy can be switched at runtime.
+    */
     public Rotation(IRotation rotationStrategy){
         this.rotationStrategy = rotationStrategy;
     }
-
+    
+    /**
+    *  The method will be called depend on different concrete strategies.
+    */
     public TetrisPiece computeNextRotation(TetrisPiece piece){
         return rotationStrategy.computeNextRotation(piece);
     }
