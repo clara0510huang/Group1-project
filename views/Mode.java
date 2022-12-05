793352a9013ea3f18342e9948e71526943c54ad2
tsa1chen <116290169+tsa1chen@users.noreply.github.com>
2022-12-04 19:25:34 -0500

    Update Mode.java

--- views/Mode.java
+++ views/Mode.java
@@ -3,13 +3,22 @@ package views;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
 
+/**
+* 
+*/
 public class Mode {
+    /**
+    * IMode strategy interface
+    */
     private IMode modeStrategy;
-
+    
     public Mode(IMode modeStrategy){
         this.modeStrategy = modeStrategy;
     }
-
+    
+    /**
+    * set the mode depend on the given concrete strategy.
+    */
     public void setMode(TetrisView view){
         modeStrategy.setMode(view);
     }
